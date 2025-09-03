42afe6f53acecf7d5db619074926658a1941ec2c
mcannistra <95096730+Cannistra94@users.noreply.github.com>
2025-09-03 16:44:54 -0400

    Update reorientation.sh

--- Preprocessing/T1_MRI/reorientation.sh
+++ Preprocessing/T1_MRI/reorientation.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# read inputs
+
+list=$1
+
+dos2unix $list
+
+for filename in `cat $list`; do
+
+fslreorient2std ../../T1/Knee_Pain/${filename}/anat.nii /autofs/space/yintang_003/users/valeria/Lab_cohorts/derivatives/T1/reo_testoldversion/reo_${filename}
+
+done
